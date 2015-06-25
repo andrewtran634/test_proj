a5c1e89d7f5f9f8107e223b14d453860ddfa7c49
Andy Tran <ajtran@umich.edu>
2015-06-24 23:33:28 -0400

    'i hope this works'

---
+++ polls/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    def __unicode__(self):
+    	return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice = models.CharField(max_length=200)
+    votes = models.IntegerField()
+    def __unicode__(self):
+    	return self.choice
